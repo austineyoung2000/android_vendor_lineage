a95253a414fbd8281ad5b1aad4df01441c82cc73
Oscar <osm1019@gmail.com>
2025-03-31 12:34:01 -0700

    Update carrier .pb to A16 beta and redo overlays

--- config/common.mk
+++ config/common.mk
@@ -405,6 +405,4 @@ include vendor/lineage/config/version.mk
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
 # Inherit from rro_overlays config
-$(call inherit-product, vendor/lineage/config/rro_overlays.mk)
-# Inherit from rro_overlays config
 $(call inherit-product, vendor/lineage/config/telephony.mk)
